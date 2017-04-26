9ff1520f6679bdde0c37d4cc445bb1d966634d33
imparikh <parikh.i.m@gmail.com>
2017-04-25 20:40:54 -0400

    fixes

--- controllers/home.js
+++ controllers/home.js
@@ -6,10 +6,8 @@ dotenv.load();
  * GET /
  */
 exports.index = function(req, res) {
-    // console.log(req.user);
-   	const coords = req.user ? (req.user.attributes.latitude ? { latitude: req.user.attributes.latitude, longitude: req.user.attributes.longitude } : null) : null;
-   	console.log(coords);
-   	res.expose(coords, "App.coordinates");
+   	const coords = req.user && req.user.attributes.latitude ? { latitude: req.user.attributes.latitude, longitude: req.user.attributes.longitude } : null;
+   	res.expose(coords, "coordinates");
     res.render('home', {
         key: process.env.API_KEY,
         title: 'Home'
